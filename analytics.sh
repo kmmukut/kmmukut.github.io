0534caabb048ebb25c538e61586bc05915c04a7c
Khaled Mosharraf Mukut <kmmukut@gmail.com>
2020-10-20 12:16:41 -0500

    Update Analytics

--- analytics.sh
+++ analytics.sh
@@ -5,7 +5,7 @@ FILES=./*.html
 for f in $FILES
 do
   echo "Processing $f file..."
-  sed '/<head>/ r gtag.js' $f  > test
+ # sed '/<head>/ r gtag.js' $f  > test
   rm $f
   cat test > $f
   rm test
