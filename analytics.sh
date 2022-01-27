9966ae65167806321a525692c7ff9b217fead5d2
Khaled Mosharraf Mukut <kmmukut@gmail.com>
2022-01-27 10:55:26 -0600

    final edits

--- analytics.sh
+++ analytics.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-git fetch 
+ 
 git pull
-FILES=./*.html
-for f in $FILES
-do
+# FILES=./*.html
+# for f in $FILES
+f=$1
+# do
   echo "Processing $f file..."
   sed '/<head>/ r gtag.js' $f  > test
   rm $f
   cat test > $f
   rm test
-done
+# done
 #sed -i '' '/mobirise.site/d' *.html
 
 git commit -a -m "Update Analytics"
